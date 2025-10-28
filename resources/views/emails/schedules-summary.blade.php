b19132cebd2136c04db28c1cd0bfe37d982f2cc3
Anderson Santos <anderson.santos@fiocruz.br>
2025-10-27 21:37:30 -0300

    Módulo de envio de notificação após criarem um agendamento

--- resources/views/emails/schedules-summary.blade.php
+++ resources/views/emails/schedules-summary.blade.php
@@ -1,6 +1,12 @@
 @component('mail::message')
-# 📆 Novos agendamentos registrados
+# 📅 Novos Agendamentos Registrados
 
 {!! nl2br(e($msg)) !!}
 
+@component('mail::button', ['url' => config('app.url')])
+Acessar o Sistema
+@endcomponent
+
+Obrigado,<br>
+{{ config('app.name') }}
 @endcomponent
