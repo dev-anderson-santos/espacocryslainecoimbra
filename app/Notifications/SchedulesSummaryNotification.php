47e5198f41aa6f6dce32ab2640a250bd0d188e95
Anderson Santos <anderson.santos@fiocruz.br>
2025-10-27 21:09:11 -0300

    Módulo de envio de notificação após criarem um agendamento

--- app/Notifications/SchedulesSummaryNotification.php
+++ app/Notifications/SchedulesSummaryNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SchedulesSummaryNotification extends Notification
+class SchedulesSummaryNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
